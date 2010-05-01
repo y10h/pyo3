85f90dc64cd63aa0da82afb882b1eb3f7fdfd994
Yury Yurevich <the.pythy@gmail.com>
2010-05-01 19:44:43 +0700

    Initial structure.

---
+++ bin/bf2cyrax.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+"""
+Migration script for move data from Byteflow instance and generate Cyrax site
+"""
---
+++ lib/lk4d4/__init__.py
@@ -0,0 +1,6 @@
+"""
+Extensions for Cyrax (http://pypi.python.org/pypi/cyrax) for pyobject.ru v3
+"""
+
+def callback(site):
+    pass
